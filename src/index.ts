470a8c1578b5de99023b92c8b4b1016008d0ad4f
s8wa2 <86946027+s8wa2@users.noreply.github.com>
2024-04-09 09:34:53 -0700

    MJS works w/ default export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { RustPlus } from "./rustplus";
-export default RustPlus; // ! CJS, MJS don't seem to support default export
+export default RustPlus; // ! CJS doesn't seem to support default export
 export * from "./proto";
 export * from "./camera";
 export { RustPlus };
